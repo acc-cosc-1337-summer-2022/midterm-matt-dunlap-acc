ef5d7df3c00dfa49716937aef1557842a1525bbe
matt-dunlap-acc <dunlap_matt@rocketmail.com>
2022-06-28 18:16:31 -0500

    midterm programming

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,56 @@
+#include "question4.h"
+#include <iostream>
+using std::cout;
+using std::cin;
 
 int main()
 {
+    bool continue_program = true;
+    char user_choice = '\0';
+    double mass = -1.0;
+    double velocity = -1.0;    
+
+    while (continue_program){
+
+        while (mass < 0){
+            cout << "\nEnter mass: ";
+            cin >> mass;
+            if (mass < 0){
+                cout << "\nInvalid input. Mass must be a positive value.";
+            }
+        }
+
+        while (velocity < 0){
+            cout << "Enter velocity: ";
+            cin >> velocity;
+            if (velocity < 0){
+                cout << "\nInvalid input. Velocity must be a positive value.";
+            }
+        }
+
+        double kinetic_energy = get_kinetic_energy(mass, velocity);
+        cout << "Kinetic energy: " << kinetic_energy;
+
+        while (user_choice != 'Y' && user_choice != 'N'){
+            cout << "\n\nRestart program? <y/n> ";
+            cin >> user_choice;
+            user_choice = toupper(user_choice);
+            if (user_choice != 'Y' && user_choice != 'N'){
+                cout << "\nInvalid input. Enter 'n' to quit or 'y' to restart the program.";
+            } else {
+                if (user_choice == 'Y'){
+                    continue_program = true;
+                } else {
+                    continue_program = false;
+                }
+            }
+        }
+
+        user_choice = '\0';
+        mass = -1.0;
+        velocity = -1.0;
+
+    }
 
     return 0;
 }
--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,5 +1,11 @@
-//
+#include "question4.h"
+
 bool test_config()
 {
     return true;
+}
+
+double get_kinetic_energy(double mass, double velocity){
+    double kinetic_energy = .5 * mass * (velocity * velocity);
+    return kinetic_energy;
 }
